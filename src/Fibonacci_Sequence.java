301f20cbfdb8b48a92c07b5c99eec541016ffbbd
GlobbyGluePlayz17 <league@WTS22.attlocal.net>
2017-07-14 12:00:47 -0700

    what
    whatwhat

---
+++ src/Fibonacci_Sequence.java
@@ -0,0 +1,23 @@
+public class Fibonacci_Sequence {
+	public static void main(String[] args) {
+		for (int i = 0; i < 13; i++) {
+			 
+		}
+	}
+}
+/*******************************************************************************
+ *    Copyright (c) The League of Amazing Programmers 2013-2017
+ *    Level 0
+ *    Fibonacci sequence
+ *    Duration=00
+ *    Platform=Eclipse
+ *    Type=Teacher
+ *    Objectives=Challenging logic
+ *    
+ *    
+ ******************************************************************************/
+
+/** 
+Print the first 12 numbers of the Fibonacci sequence			
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144		
+*/
